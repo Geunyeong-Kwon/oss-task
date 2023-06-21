7258e29aa1679a0a3f364878e206be168edc4c69
Geunyeong-Kwon <124476489+Geunyeong-Kwon@users.noreply.github.com>
2023-06-22 08:55:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,12 @@ import pandas as pd
 import numpy as np
 import graphviz as grphviz
 import matplotlib.pyplot as plt
+
+st.write('#Youtube view')
+st.write('## raw')
+view=[100,1500,30]
+view
+st.write('##bar chart')
+st.bar_chart(view)
+sview=pd.Series(view)
+sview
