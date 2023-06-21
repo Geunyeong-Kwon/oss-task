aa8e391933669eaf44d9142d893cb81359d8d910
Geunyeong-Kwon <124476489+Geunyeong-Kwon@users.noreply.github.com>
2023-06-22 08:57:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import graphviz as grphviz
+import graphviz as graphviz
 import matplotlib.pyplot as plt
 
 st.write('#Youtube view')
