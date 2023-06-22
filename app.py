89cc54e300b57a7c2fe35a4277988450da5b81e2
Geunyeong-Kwon <124476489+Geunyeong-Kwon@users.noreply.github.com>
2023-06-22 10:41:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import requests
 
 st.title("독거 노인 수")
 url = 'http://apis.data.go.kr/1352000/ODMS_STAT_24/callStat24Api'
-params ={'serviceKey' : '서비스키', 'pageNo' : '1', 'numOfRows' : '10', 'apiType' : 'XML', 'year' : '2019' }
+params ={'serviceKey' : 'http://apis.data.go.kr/1352000/ODMS_STAT_24', 'pageNo' : '1', 'numOfRows' : '10', 'apiType' : 'XML', 'year' : '2019' }
 
 response = requests.get(url, params=params)
 print(response.content)
