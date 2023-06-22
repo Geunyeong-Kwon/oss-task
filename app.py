0b4a848d27e21f39a096fc688287e8588be39cda
Geunyeong-Kwon <124476489+Geunyeong-Kwon@users.noreply.github.com>
2023-06-22 09:36:29 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import pandas as pd
+import json
+import urllib.request
 
-st.write('#Youtube view')
-st.write('## raw')
-view=[100,1500,30]
-view
-st.write('##bar chart')
-st.bar_chart(view)
-sview=pd.Series(view)
-sview
+key = 'KU4sbUhkqLCoOaTz0eNuf5DBCORUzbcL7XaHhqi4qSY2%2Bs%2FFvwIGBBiG4tbIxRI%2F1ftQTPvmeQstoiwXt4I2tw%3D%3D'
+url = 'http://apis.data.go.kr/1352000/ODMS_STAT_24/callStat24Api' + key + http://apis.data.go.kr/1352000/ODMS_STAT_24
+json_page = urllib.request.urlopen(url)
+json_data = json_page.read().decode("utf-8")
+json_array = json.loads(json_data)
+
+print(json_array)
