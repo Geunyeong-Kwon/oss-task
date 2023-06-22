0de52b0b915a0bbea8ecf89bfc0905ddcbd634cd
Geunyeong-Kwon <124476489+Geunyeong-Kwon@users.noreply.github.com>
2023-06-22 10:17:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,46 @@
 import streamlit as st
 import pandas as pd
-import json
-import urllib.request
+import requests
 
-key = 'KU4sbUhkqLCoOaTz0eNuf5DBCORUzbcL7XaHhqi4qSY2%2Bs%2FFvwIGBBiG4tbIxRI%2F1ftQTPvmeQstoiwXt4I2tw%3D%3D'
-url = 'http://apis.data.go.kr/1352000/ODMS_STAT_24/callStat24Api' + key + http://apis.data.go.kr/1352000/ODMS_STAT_24
-json_page = urllib.request.urlopen(url)
-json_data = json_page.read().decode("utf-8")
-json_array = json.loads(json_data)
+# API 데이터 가져오기
+def get_data(api_key, param1, param2):
+    url = f'KU4sbUhkqLCoOaTz0eNuf5DBCORUzbcL7XaHhqi4qSY2%2Bs%2FFvwIGBBiG4tbIxRI%2F1ftQTPvmeQstoiwXt4I2tw%3D%3D?param1={param1}&param2={param2}&api_key={api_key}'  # 실제 API 엔드포인트로 수정
+    response = requests.get(url)
+    data = response.json()
+    return data
 
-print(json_array)
+# streamlit 애플리케이션 생성
+def main():
+    # 애플리케이션 제목
+    st.title('조건에 따른 차트와 지도 출력')
+
+    # 입력 위젯
+    param1 = st.selectbox('파라미터1 선택', ['값1', '값2', '값3'])
+    param2 = st.slider('파라미터2 선택', 0, 100, (0, 100))
+
+    # API 데이터 가져오기
+    api_key = 'KU4sbUhkqLCoOaTz0eNuf5DBCORUzbcL7XaHhqi4qSY2%2Bs%2FFvwIGBBiG4tbIxRI%2F1ftQTPvmeQstoiwXt4I2tw%3D%3D'  
+    data = get_data(api_key, param1, param2)
+
+    # 데이터 처리 및 시각화
+    df = pd.DataFrame(data)  # API 응답 데이터를 데이터프레임으로 변환
+    filtered_data = process_data(df)  # 데이터 처리 함수에 따라 가공
+
+    # 차트 또는 지도 출력
+    if len(filtered_data) > 0:
+        st.subheader('출력 결과')
+        if st.checkbox('차트 출력'):
+            st.line_chart(filtered_data)
+        if st.checkbox('지도 출력'):
+            show_map(filtered_data)
+    else:
+        st.subheader('출력 결과')
+        st.write('조건에 해당하는 데이터가 없습니다.')
+
+# 지도 출력 함수
+def show_map(data):
+    # 지도 생성 및 마커 추가 작업
+    # ...
+
+if __name__ == '__main__':
+    main()
